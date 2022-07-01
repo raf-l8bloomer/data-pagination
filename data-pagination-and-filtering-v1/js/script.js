2add6986e0685af0a7dfdd81c4d6908f36442487
rafael.lumang <lumang.rafael@gmail.com>
2022-07-01 15:37:30 -0700

    Set variables but need to figure out Search function

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -9,7 +9,96 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+const header = document.querySelector('.header');
 
+/* NEED createElement function TO CLEAN CODE*/
+
+const label = document.createElement('label');
+label.for = 'search';
+label.className = 'student-search';
+header.appendChild(label);
+
+const span = document.createElement('span');
+span.textContent = 'Search by name';
+label.appendChild(span);
+
+const input = document.createElement('input');
+input.id = 'search';
+input.placeholder = 'Search by name...';
+label.appendChild(input);
+
+const button = document.createElement('button');
+button.type = 'button';
+label.appendChild(button);
+
+const buttonImg = document.createElement('img');
+buttonImg.src = 'img/icn-search.svg';
+buttonImg.alt = 'Search icon';
+button.appendChild(buttonImg);
+
+const searchInput = input.value;
+
+/*NEED TO FIGURE OUT THIS SEARCH MATCHING THANG*/
+
+function searchName (search, names) {
+   for (let i = 0; i < names.length; i++){
+   if (search.length != 0 && names[i].textContent.toLowerCase().includes(search.toLowerCase())){
+      showPage(names, 1);
+        }
+   }
+}
+
+button.addEventListener ('click', () => {
+   searchName (searchInput, data);
+   console.log('Submit works!');
+});
+
+label.addEventListener ('keyup', () => {
+   searchName (searchInput, data);
+   console.log('Typing works!');
+   console.log(search);
+});
+// const search = document.createElement('input');
+// search.type = 'text';
+// search.value = '';
+// search.className = 'student-search';
+// search.placeholder='Search student';
+// search.id = "search-input";
+// form.appendChild(search);
+
+// const submit = document.createElement('input');
+// submit.type = 'submit';
+// submit.className = 'student-search'
+// submit.textContent = 'Submit';
+// form.appendChild(submit);
+
+{/* <label for="search" class="student-search">
+  <span>Search by name</span>
+  <input id="search" placeholder="Search by name...">
+  <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label> */}
+
+
+
+
+// let searchInput = search.value;
+
+// // let i = 0;
+
+// // const studentNames = `${data[i].name.first}`;
+
+
+
+
+//    }
+
+// }
+
+
+
+
+
+// access name like data[i].name.first
 
 /*
 Create the `showPage` function
@@ -22,6 +111,7 @@ This function will create and insert/append the elements needed to display a "pa
 function showPage(list, page) {
    const startIndex = (page * 9 ) - 9;
    const endIndex = page*9;
+
    const studentList = document.querySelector('ul'); 
    studentList.innerHTML = ''; // removes existing students displayed previously
 
@@ -55,43 +145,37 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 
-/* addPagination creates # of pages needed to present  9 students( index:0-9 ) per page
-*/
+/* addPagination creates # of pages needed to present  9 students( index:0-9 ) per page */
 function addPagination(list) {
    const numOfPages = Math.ceil(list.length/9); // 
    const linkList = document.querySelector('.link-list')
-   // set the innerHTML property of the variable you just created to an empty string
    linkList.innerHTML = "" // removes existing pages displayed previously
-   // loop over the number of pages needed
+   /*loops over number of pages needed and creates and numbers buttons needed */
    for (let i = 1; i <= numOfPages; i++) {
-     // create the elements needed to display the pagination button
      const button = `
       <li>
       <button type="button">${[i]}</button>
       </li>
       `
-     // insert the above elements
+     /* insert the buttons to the link-list within pagination div*/
      linkList.insertAdjacentHTML("beforeend", button)
    }
-   // give the first pagination button a class of "active"
+   /*first button is always active*/
    const firstButton = document.querySelector('button');
    firstButton.className ='active';
  
-   // create an event listener on the `link-list` element
-
+   /* when buttons are clicked, removes current active class and adds to clicked button */
    linkList.addEventListener ('click', (e) => {
       if (e.target.tagName === 'BUTTON') {
         const removePrevBtn = document.querySelector('.active');
         removePrevBtn.className = '';
         const activeBtn = e.target;
         activeBtn.className = 'active';
+
+        /*shows page according to active page # button clicked */
         showPage(list, activeBtn.textContent)
       }
    })
-     // if the click target is a button:
-       // remove the "active" class from the previous button
-       // add the active class to the clicked button
-       // call the showPage function passing the `list` parameter and page to display as arguments
  }
 
 // Call functions

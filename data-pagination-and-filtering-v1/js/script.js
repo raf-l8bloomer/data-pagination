daf347aa44b9f1a1ef1f5455e98ebb2594a0afbd
rafael.lumang <lumang.rafael@gmail.com>
2022-07-01 13:33:21 -0700

    pagination code working *meeting expectations*

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -3,8 +3,6 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-console.log(data);
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
@@ -19,6 +17,37 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 
+/* showPage pulls all elements in list parameter and sets how many elements to present per 1 page */
+
+function showPage(list, page) {
+   const startIndex = (page * 9 ) - 9;
+   const endIndex = page*9;
+   const studentList = document.querySelector('ul'); 
+   studentList.innerHTML = ''; // removes existing students displayed previously
+
+   /*loops over list parameter displaying 9 students( index:0-9 ) within if statement */
+   for (let i = 0; i < list.length; i++ ) {
+     if (i >= startIndex && i < endIndex) {
+        const studentItem = `
+         <li class="student-item cf">
+         <div class="student-details">
+            <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+            <h3>${data[i].name.first} ${data[i].name.last}</h3>
+            <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+            <span class="date">${data[i].registered.date}</span>
+         </div>
+         </li>
+        `;
+
+         /* insert listed studentItems inside studentList ul */
+         studentList.insertAdjacentHTML('beforeend', studentItem);
+
+     }
+   }
+ }
+
 
 /*
 Create the `addPagination` function
@@ -26,5 +55,46 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 
+/* addPagination creates # of pages needed to present  9 students( index:0-9 ) per page
+*/
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length/9); // 
+   const linkList = document.querySelector('.link-list')
+   // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = "" // removes existing pages displayed previously
+   // loop over the number of pages needed
+   for (let i = 1; i <= numOfPages; i++) {
+     // create the elements needed to display the pagination button
+     const button = `
+      <li>
+      <button type="button">${[i]}</button>
+      </li>
+      `
+     // insert the above elements
+     linkList.insertAdjacentHTML("beforeend", button)
+   }
+   // give the first pagination button a class of "active"
+   const firstButton = document.querySelector('button');
+   firstButton.className ='active';
+ 
+   // create an event listener on the `link-list` element
+
+   linkList.addEventListener ('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+        const removePrevBtn = document.querySelector('.active');
+        removePrevBtn.className = '';
+        const activeBtn = e.target;
+        activeBtn.className = 'active';
+        showPage(list, activeBtn.textContent)
+      }
+   })
+     // if the click target is a button:
+       // remove the "active" class from the previous button
+       // add the active class to the clicked button
+       // call the showPage function passing the `list` parameter and page to display as arguments
+ }
 
 // Call functions
+
+showPage(data,1);
+addPagination(data);
